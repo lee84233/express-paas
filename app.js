9d9f0a6577fce00754cf4ff709543a992ec27697
lee84233 <842337932@qq.com>
2019-10-28 14:19:58 +0800

    update

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const proxyMiddleWare = require('http-proxy-middleware');
 
 // 环境变量：development | product
 const ENV = 'development';
-
 // 端口
-const PORT = 80;
+const PORT = 8080;
+
 // 代理地址
 var proxy_url = '';
-
 if (ENV === 'development') {
   // 开发环境
   proxy_url = 'https://test.yiger.com';
@@ -20,16 +19,22 @@ if (ENV === 'development') {
   proxy_url = "https://www.yiger.com";
 }
 
-// 解决前端路由history模式下404
-// 默认加载index.html
+/**
+ * 解决前端路由history模式404问题
+ * 默认加载index.html
+ */
 app.use(history({
   index: '/index.html'
 }));
 
-// 前端资源目录
+/**
+ * 前端资源目录
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 代理
+/**
+ * 代理
+ */
 app.use(
   '/api',
   proxyMiddleWare({
